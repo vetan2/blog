b06444e44386f40be984d075c4c48edec55d90dc
vetan2 <vetan2@naver.com>
2023-03-21 21:03:32 +0900

    Add schema, gql

--- apps/strapi/config/plugins.ts
+++ apps/strapi/config/plugins.ts
@@ -6,4 +6,17 @@ export default () => ({
       },
     },
   },
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
 })
